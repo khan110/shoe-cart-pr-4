3cb9c1ed1f3ce915f26f2e25e33f707a5bba2aec
Umair ali khan <umair_kh90@yahoo.com>
2020-07-15 02:57:42 -0500

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div >
       <h1>Welcome to Shoe Store</h1>
-      <img src="https://static.highsnobiety.com/thumbor/IfqSUor8iNJiwuf_xTWczaJDhl8=/fit-in/1200x800/smart/static.highsnobiety.com/wp-content/uploads/2019/05/20103752/best-sneaker-stores-new-york-2.jpg" alt={myc}/>
+      <img src="https://static.highsnobiety.com/thumbor/IfqSUor8iNJiwuf_xTWczaJDhl8=/fit-in/1200x800/smart/static.highsnobiety.com/wp-content/uploads/2019/05/20103752/best-sneaker-stores-new-york-2.jpg" alt="mainimg"/>
     </div>
   );
 }
